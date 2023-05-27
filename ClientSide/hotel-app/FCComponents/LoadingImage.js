46399c2f76e872c869c72b1b6d3eb25a6bb19cec
DanielFigman <118261654+DanielFigman@users.noreply.github.com>
2023-05-27 11:56:55 +0300

    added images to the cache

--- ClientSide/hotel-app/FCComponents/LoadingImage.js
+++ ClientSide/hotel-app/FCComponents/LoadingImage.js
@@ -1,26 +1,84 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import { Image } from 'react-native'
+import React, { useEffect, useState } from 'react';
+import { Image } from 'react-native';
+import * as FileSystem from 'expo-file-system';
+
+const imageCache = {};
 
 const LoadingImage = ({ imageURL, style }) => {
+  const [cachedImageURI, setCachedImageURI] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    const cacheImage = async () => {
+      if (!imageURL) {
+        setError(new Error('Image URL is missing'));
+        setIsLoading(false);
+        return;
+      }
 
-    const [image, setImage] = useState(require('../assets/imageLoading.gif'))
-    const [isLoading, setIsLoading] = useState(true)
+      if (imageCache[imageURL]) {
+        // Image is already cached, use the cached URI
+        setCachedImageURI(imageCache[imageURL]);
+        setIsLoading(false);
+        return;
+      }
 
-    useEffect(() => {
-        setIsLoading(true);
-        setImage(require('../assets/imageLoading.gif'))
-      }, []);
-    
+      const fileName = getFileNameFromURL(imageURL);
+      const cachedImagePath = FileSystem.cacheDirectory + fileName;
 
-    return (
-        <Image
-            style={style}
-            onLoad={() => setIsLoading(false)}
-            source={isLoading ? image : { uri: imageURL }}
-        />
-    )
-}
+      try {
+        await FileSystem.getInfoAsync(cachedImagePath);
+        // File exists in cache, attempt to delete it
+        await FileSystem.deleteAsync(cachedImagePath, { idempotent: true });
+        console.log('Cache cleared successfully');
+      } catch (error) {
+        console.log('Failed to clear cache:', error);
+      }
 
-export default LoadingImage
+      try {
+        const { uri } = await FileSystem.downloadAsync(imageURL, cachedImagePath);
+        // Cache the downloaded image URI
+        imageCache[imageURL] = uri;
+        setCachedImageURI(uri);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error caching image:', error);
+        setError(error);
+        setIsLoading(false);
+      }
+    };
+
+    cacheImage();
+  }, [imageURL]);
+
+  const getFileNameFromURL = (url) => {
+    if (!url) {
+      return 'image.jpg';
+    }
+
+    const matches = url.match(/\/([^/]+)$/);
+    if (matches && matches.length > 1) {
+      return matches[1];
+    }
+    return 'image.jpg';
+  };
+
+  if (isLoading) {
+    return <Image style={style} source={require('../assets/imageLoading.gif')} />;
+  }
+
+  if (error) {
+    // Handle the error case here
+    return <Image style={style} source={require('../assets/errorImage.png')} />;
+  }
+
+  if (cachedImageURI) {
+    return <Image style={style} source={{ uri: cachedImageURI }} />;
+  }
+
+  // Handle any other fallback case here
+  return null;
+};
+
+export default LoadingImage;
