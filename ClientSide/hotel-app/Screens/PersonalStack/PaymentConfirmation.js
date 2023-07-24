3525414ce401cb6123304fee1ef197c1cb67e6fb
DanielFigman <118261654+DanielFigman@users.noreply.github.com>
2023-07-25 01:26:22 +0300

    fixed

--- ClientSide/hotel-app/Screens/PersonalStack/PaymentConfirmation.js
+++ ClientSide/hotel-app/Screens/PersonalStack/PaymentConfirmation.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View, Image } from "react-native";
-import React from "react";
+import React, { useContext } from "react";
 import { CheckCircleIcon } from "react-native-heroicons/outline";
 import ScreenComponent from "../../FCComponents/ScreenComponent";
 import ButtonMain from "../../FCComponents/Buttons";
