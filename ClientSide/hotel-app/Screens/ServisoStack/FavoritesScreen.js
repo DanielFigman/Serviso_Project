93299d7c2a1a8903cd8b55d8bb79f631672e67a6
DanielFigman <118261654+DanielFigman@users.noreply.github.com>
2023-07-25 10:06:07 +0300

    hotFix

--- ClientSide/hotel-app/Screens/ServisoStack/FavoritesScreen.js
+++ ClientSide/hotel-app/Screens/ServisoStack/FavoritesScreen.js
@@ -1,96 +1,82 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
-import React, { useContext, useEffect, useState } from "react";
-import { HotelsAppContext } from "../../Context/HotelsAppContext";
-import ScreenComponent from "../../FCComponents/ScreenComponent";
-import HotelActivityCarouselCard from "../../FCComponents/Cards/HotelActivityCarouselCard";
-import ConciergeCarouselCard from "../../FCComponents/Cards/ConciergeCarouselCard";
-import Languages from "../../Json_files/Languages";
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
+import { HotelsAppContext } from '../../Context/HotelsAppContext'
+import ScreenComponent from '../../FCComponents/ScreenComponent'
+import HotelActivityCarouselCard from '../../FCComponents/Cards/HotelActivityCarouselCard'
+import ConciergeCarouselCard from '../../FCComponents/Cards/ConciergeCarouselCard'
 
 const FavoritesScreen = () => {
-  const { updatedActivities, activities_hotel, activities_nearBy } =
-    useContext(HotelsAppContext);
-  const [cardsToShow, setCardsToShow] = useState([]);
-  const { language } = useContext(HotelsAppContext);
-  const screenContent = Languages.FavoritesScreen;
 
-  const generateCardsToShow = () => {
-    let setVal = [];
+    const { updatedActivities, activities_hotel, activities_nearBy } = useContext(HotelsAppContext)
+    const [cardsToShow, setCardsToShow] = useState([])
 
-    const likedIds = updatedActivities
-      .filter((obj) => obj.favorite)
-      .map((obj) => obj.placeID);
 
-    let activitiesHotelToSet = activities_hotel.filter((obj) =>
-      likedIds.includes(obj.placeID)
-    );
-    let activitiesNearBy = activities_nearBy.filter((obj) =>
-      likedIds.includes(obj.placeID)
-    );
 
-    // Add the `type` prop to each object in `retVal`
-    setVal = setVal
-      .concat(
-        activitiesHotelToSet.map((item) => ({
-          ...item,
-          type: "activities_hotel",
-        })),
-        activitiesNearBy.map((item) => ({ ...item, type: "activities_nearBy" }))
-      )
-      .flat();
+    const generateCardsToShow = () => {
+        let setVal = [];
 
-    setCardsToShow(setVal);
-  };
+        const likedIds = updatedActivities.filter(obj => obj.favorite).map(obj => obj.placeID);
 
-  useEffect(() => {
-    generateCardsToShow();
-  }, [updatedActivities]);
+        let activitiesHotelToSet = activities_hotel.filter(obj => likedIds.includes(obj.placeID));
+        let activitiesNearBy = activities_nearBy.filter(obj => likedIds.includes(obj.placeID));
 
-  const renderItems = () => {
+
+        // Add the `type` prop to each object in `retVal`
+        setVal = setVal.concat(
+            activitiesHotelToSet.map(item => ({ ...item, type: 'activities_hotel' })),
+            activitiesNearBy.map(item => ({ ...item, type: 'activities_nearBy' }))
+        ).flat();
+
+        setCardsToShow(setVal);
+    };
+
+    useEffect(() => {
+        generateCardsToShow();
+
+    }, [updatedActivities])
+
+    const renderItems = () => {
+        return (
+            <ScrollView>
+                <View style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", paddingBottom: 90 }}>
+                    {cardsToShow?.map((item, index) => (
+                        <View key={item.placeID + index + item.type} style={{ width: "50%" }}>
+                            {
+                                item.type === "activities_hotel" ?
+                                    <HotelActivityCarouselCard id={index} item={item} />
+                                    :
+                                    <ConciergeCarouselCard id={index} item={item} />
+                            }
+                        </View>
+                    ))}
+                </View>
+            </ScrollView>
+        );
+    }
+
+
     return (
-      <ScrollView>
-        <View
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            flexWrap: "wrap",
-            paddingBottom: 90,
-          }}
-        >
-          {cardsToShow?.map((item, index) => (
-            <View
-              key={item.placeID + index + item.type}
-              style={{ width: "50%" }}
-            >
-              {item.type === "activities_hotel" ? (
-                <HotelActivityCarouselCard id={index} item={item} />
-              ) : (
-                <ConciergeCarouselCard id={index} item={item} />
-              )}
-            </View>
-          ))}
-        </View>
-      </ScrollView>
-    );
-  };
+        <ScreenComponent title={
+            <Text
+                style={{
+                    fontWeight: 'bold',
+                    fontSize: 30,
+                    marginHorizontal: 100
+                }}>
+                Favorites
+            </Text>
+        }
+            content={
+                <View>
+                    {
+                        renderItems()
+                    }
+                </View>
+            } />
 
-  return (
-    <ScreenComponent
-      title={
-        <Text
-          style={{
-            fontWeight: "bold",
-            fontSize: 30,
-            marginHorizontal: 100,
-          }}
-        >
-          {screenContent.Favorites[language]}
-        </Text>
-      }
-      content={<View>{renderItems()}</View>}
-    />
-  );
-};
+    )
+}
 
-export default FavoritesScreen;
+export default FavoritesScreen
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({})
